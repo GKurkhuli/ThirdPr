b67394c26c7e4919f7932f9906ca436258d718b8
GKurkhuli <giorgi.kurkhuli1@gmail.com>
2020-08-17 08:49:16 +0400

    upd

--- src/js/script.js
+++ src/js/script.js
@@ -19,49 +19,31 @@ $('#buttonStartCall').hover(
         .then(function(){
             document.querySelector("#buttonStartCall").style.opacity = '1';
             document.querySelector("#buttonEndCall").style.opacity = '0.6';
-            if(!callAllowed){
-                var deviceArray = [];
-                navigator.mediaDevices.enumerateDevices()
-                .then(function(devices) {
-                    devices.forEach(function(device) {
-                        deviceArray.push(device.kind)
-                });
-                    if(deviceArray.includes("audioinput"))
-                    {
-                        document.querySelector("#buttonStartCall").style.opacity = '0.6';
-                        callAllowed = true;
-                    }
-                    else{
-                        document.querySelector(".call__allert__text__content").innerHTML = "There was no audio input device found";
-                        $('#alertMPdisabled').show("fast");
-                        callAllowed = false;
-                    }
-                }); 
-            }             
+            if(!callAllowed)
+                getDevices();
         })
         .catch(function(){
             document.querySelector("#buttonStartCall").style.opacity = '0.6';
             document.querySelector(".call__allert__text__content").innerHTML = "This sometimes happens when microphone access has been disabled";
-            $('#alertMPdisabled').show("fast");
+            $('#alertMPdisabled').show(100);
             callAllowed = false;
         });
     },
     function(){
-        $('#alertMPdisabled').hide("fast");
+        $('#alertMPdisabled').hide(100);
     }
 )
 
 $('#buttonStartCall').click(function(){
     if(callAllowed)
-    {   
+    {
         document.querySelector('#buttonStartCall').style.opacity='0.6';
         document.querySelector('#buttonEndCall').style.opacity='1';
         for(var i = 0; i < 3; i++)
-            $('.caller__avatar__circle_'+i).show("slow");
+            $('.caller__avatar__circle_'+i).show(500);
         totalSeconds = 0;
         timeInterval=setInterval(timer,1000);//update timer each second   
         callInProgress = true;
-
     }
 });
 
@@ -74,10 +56,34 @@ function endCall(){
     document.querySelector('#buttonStartCall').style.opacity='1';
     document.querySelector('#buttonEndCall').style.opacity='0.6';
     for(var i = 0; i < 3; i++)
-        $('.caller__avatar__circle_'+i).hide("slow");
+        $('.caller__avatar__circle_'+i).hide(500);
     clearInterval(timeInterval);  
     document.querySelector('#time').innerHTML = "00:00";
     callInProgress = false;
+}
+
+var deviceArray = [];
+function getDevices()
+{
+    deviceArray = [];
+    navigator.mediaDevices.enumerateDevices()
+    .then(function(devices) {
+        devices.forEach(function(device) {
+            deviceArray.push(device.kind)
+    });
+        if(deviceArray.includes("audioinput"))
+            callAllowed = true;
+        else{
+            document.querySelector("#buttonStartCall").style.opacity = '0.6';
+            document.querySelector(".call__allert__text__content").innerHTML = "There was no audio input device found";
+            $('#alertMPdisabled').show("fast");
+            callAllowed = false;
+        }
+    }); 
+}
+
+navigator.mediaDevices.ondevicechange = function(event){
+    getDevices();
 }
 
 var totalSeconds;
